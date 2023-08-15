9afc2b064711737f7f2dd7e2e70addef12ed000e
tigca <84330292+tigca@users.noreply.github.com>
2023-08-15 14:14:14 +0300

    Create compliments.py

---
+++ compliments.py
@@ -0,0 +1,25 @@
+# #️⃣ Module "compliments" by t.me/wavams
+# ✅ Only for teagram userbot 
+# ⚙️ Commands: .compliments
+from pyrogram import Client, types
+from .. import loader, utils
+import random
+
+
+@loader.module(name="Compliments")
+class ComplimentsMod(loader.Module):
+    """👻 Модуль с рандомными комплиментами"""
+
+
+    @loader.ratelimit
+    async def complimentscmd(self, app: Client, message: types.Message):
+        compliments = [
+            "Ты лучший!",
+            "Не сдавайся!",
+            "У тебя все получится!",
+            "Ты умный и талантливый!",
+            "Ты прекрасен!",
+            "Ты делаешь мир лучше!"
+        ]
+        compliment = random.choice(compliments)
+        await utils.answer(message, compliment)
